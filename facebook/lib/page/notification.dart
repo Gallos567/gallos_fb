ac67c54c101f367e40462328df5cc72ba9207bcb
DiazDaizelle <daizellenicole.diaz-22@cpu.edu.ph>
2025-10-24 10:38:33 -0700

    midterm_exam_gallos

--- facebook/lib/page/notification.dart
+++ facebook/lib/page/notification.dart
@@ -10,6 +10,69 @@ class NotificationPage extends StatefulWidget {
 class _NotificationPageState extends State<NotificationPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Notifications", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),),
+        actions: [
+          CircleAvatar(
+            backgroundColor: Colors.grey[300],
+            child: IconButton(onPressed: () {}, icon: Icon(Icons.more_horiz))
+          ),
+          CircleAvatar(
+            backgroundColor: Colors.grey[300],
+            child: IconButton(onPressed: () {}, icon: Icon(Icons.search))
+          )
+        ],),
+        body: const SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 18),
+                child: Text("Today", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold))),
+              ListTile(
+                tileColor: Color.fromARGB(255, 136, 213, 243),
+                trailing: Icon(Icons.more_horiz),
+                leading: CircleAvatar(
+                  backgroundImage: AssetImage("assets/myday/myday1.jpg"),
+                ),
+                title: Text("Learn how Meta will use your info in new ways to personalize your experiences"),
+                subtitle: Text("16h"),
+              ),
+              ListTile(
+                tileColor: Color.fromARGB(255, 136, 213, 243),
+                trailing: Icon(Icons.more_horiz),
+                leading: CircleAvatar(
+                  backgroundImage: AssetImage("assets/myday/myday2.jpg"),
+                ),
+                title: Text("5 Unknown Apps recently shared 1 post"),
+                subtitle: Text("20h"),
+              ),
+              ListTile(
+                tileColor: Color.fromARGB(255, 136, 213, 243),
+                trailing: Icon(Icons.more_horiz),
+                leading: CircleAvatar(
+                  backgroundImage: AssetImage("assets/myday/myday4.jpg"),
+                ),
+                title: Text("Fandango posted a new reel: ''Diego Luna, Jennifer Lopez and more stars celebrate Hispanic Heritage Month''"),
+                subtitle: Text("2h"),
+              ),
+              SizedBox(width: 10),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 18),
+                child: Text("Earlier", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold))),
+              ListTile(
+                tileColor: Color.fromARGB(255, 136, 213, 243),
+                trailing: Icon(Icons.more_horiz),
+                leading: CircleAvatar(
+                  backgroundImage: AssetImage("assets/myday/myday5.jpg"),
+                ),
+                title: Text("9GAG posted a new reel: ''White cat licks black cat''"),
+                subtitle: Text("1d"),
+              ),
+            ],
+          ),
+        )
+    );
   }
 }
